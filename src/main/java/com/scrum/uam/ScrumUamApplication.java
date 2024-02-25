60174a06175177c527094daef278fd28fee00df2
Akhil Vudatha <akhilvsvs1999@gmail.com>
2024-02-26 01:42:11 +0530

    Initial Commit

---
+++ src/main/java/com/scrum/uam/ScrumUamApplication.java
@@ -0,0 +1,13 @@
+package com.scrum.uam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumUamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumUamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrum/uam/ScrumUamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrum.uam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumUamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
